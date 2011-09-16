bfa8a7b045f2591ac72c16daadd0ab4870bd32dd
Jack Danger Canty <jackdanger@squareup.com>
2011-09-15 17:00:26 -0700

    allowing indenting

--- lib/marked.rb
+++ lib/marked.rb
@@ -1,6 +1,7 @@
 module Marked
   def mark *objects
 
+    Marked.indent_out
     Marked.log "\nMARKED #{caller.first.split(':in ').first}"
 
     returnable = if block_given?
@@ -22,28 +23,47 @@ module Marked
 
     Marked.rails_log " / FINISHED MARKING"
 
+    Marked.indent_in
     returnable
   end
 
-  def self.log object
-    rails_log object
-    print object
-  end
+  class << self
+    def log object
+      rails_log object
+      print object
+    end
 
-  def self.rails_log object
-    Rails.logger.debug object if defined?(Rails)
-  end
+    def rails_log object
+      Rails.logger.debug object if defined?(Rails)
+    end
 
-  def self.print object
-    STDOUT.puts object
-  end
+    def print object
+      STDOUT.puts object
+    end
 
-  def self.pad object
-    "       " + (object.is_a?(String) ? object : object.inspect)
-  end
+    def pad object
+      "       " + (" " * indent) + (object.is_a?(String) ? object : object.inspect)
+    end
 
-  def self.print_benchmark measurement
-    log pad "* Executed in #{'%0.3f' % measurement.total} seconds (#{'%0.3f' % measurement.utime} cpu)\n"
+    def print_benchmark measurement
+      log pad "* Executed in #{'%0.3f' % measurement.total} seconds (#{'%0.3f' % measurement.utime} cpu)\n"
+    end
+
+    def indent
+      @indent ||= 0
+    end
+
+    def indent= integer
+      @indent = integer
+    end
+
+    def indent_out
+      @indent = indent + 1
+    end
+
+    def indent_in
+      @indent = indent - 1
+    end
   end
 end
 
